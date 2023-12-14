4429e1d35aa4364f6583197b92ef1cd52a99f9a6
moris96 <moriskhoudari@gmail.com>
2023-12-14 14:52:23 -0500

    working on animated background component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import AnimateBackground from "./components/AnimateBackground/AnimateBackground";
+
 
 
 function App() {
   return (
     <div className="App">
       <h1>URL Shortener App</h1>
+      <AnimateBackground/>
     </div>
   );
 }
---
+++ src/components/AnimateBackground/AnimateBackground.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AnimateBackground.css'
+
+const AnimateBackground = () => {
+  return (
+    <div className='background'>
+      <div className='wave -1' />
+      <div className='wave -2' />
+      <div className='wave -3' />
+    </div>
+  )
+}
+
+export default AnimateBackground
